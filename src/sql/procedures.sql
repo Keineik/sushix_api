13f55b8c8883ef4440b6d629b5b7809190f0e9ca
Lê Ngọc Vĩ <vile2040@gmail.com>
2024-12-12 18:49:45 +0700

    Fix bugs

--- src/sql/procedures.sql
+++ src/sql/procedures.sql
@@ -9,29 +9,42 @@ GO
 create proc sp_FetchItems
     @Page int = 1,
     @Limit int = 18,
-    @SearchTerm nvarchar(100) = '', --ItemID or ItemName
-    @CategoryID int = 0, --Filter
-    @BranchID int = 0,--Filter
-    @SortKey varchar(10) = 'ID', --Price or ID
-    @SortDirection bit = 0 --0: asc, 1: desc
+    @SearchTerm nvarchar(100) = '', -- ItemID or ItemName
+    @CategoryID int = 0, -- Filter
+    @BranchID int = 0, -- Filter
+    @SortKey varchar(10) = 'ID', -- Price or ID
+    @SortDirection bit = 0 -- 0: asc, 1: desc
 as
 begin
-
+	SET NOCOUNT ON;
+	
     declare @Offset int = (@Page - 1) * @Limit;
     declare @Search nvarchar(100) = '%' + @SearchTerm + '%';
 
-    select * 
-    from BranchMenuItem bmi join MenuItem mi on bmi.ItemID = mi.ItemID
-    where mi.ItemName LIKE @Search
-      and (@CategoryID = 0 or mi.CategoryID = @CategoryID)
-      and (@BranchID = 0 or bmi.BranchID = @BranchID)
-    order by 
-      case when @SortKey = 'price' AND @SortDirection = 0 then mi.UnitPrice end asc,
-	  case when @SortKey = 'price' and @SortDirection = 1 then mi.UnitPrice end desc,
-      case when @SortKey = 'ID' and @SortDirection = 0 then mi.ItemID end asc,
-	  case when @SortKey = 'ID' and @SortDirection = 1 then mi.ItemID end desc
+	select mi.ItemID,
+		mi.ItemName,
+		mi.UnitPrice,
+		mi.ServingUnit,
+		mi.CategoryID,
+		mi.IsDiscontinued,
+		mi.ImgUrl
+	from MenuItem mi left join BranchMenuItem bmi on bmi.ItemID = mi.ItemID
+	where (mi.ItemName like @Search or mi.ItemID like @Search)
+		and (@CategoryID = 0 or mi.CategoryID = @CategoryID)
+		and (@BranchID = 0 or bmi.BranchID = @BranchID)
+	group by mi.ItemID, mi.ItemName, mi.UnitPrice, mi.ServingUnit, mi.CategoryID, mi.IsDiscontinued, mi.ImgUrl
+	order by 
+    case 
+        when @SortKey = 'price' AND @SortDirection = 0 then UnitPrice
+        when @SortKey = 'ID' AND @SortDirection = 0 then mi.ItemID
+    end asc,
+    case 
+        when @SortKey = 'price' AND @SortDirection = 1 then UnitPrice
+        when @SortKey = 'ID' AND @SortDirection = 1 then mi.ItemID
+    end desc
     offset @Offset rows fetch next @Limit rows only;
-END;
+    
+end;
 GO
 exec sp_FetchItems
 GO
