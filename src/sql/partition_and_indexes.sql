25c3e1408b6c94224eec5c2eca937e4a7ac2306d
Keineik <duckienvanba@gmail.com>
2025-01-01 13:06:40 +0700

    added partition statistics and fix loading csv file

--- src/sql/partition_and_indexes.sql
+++ src/sql/partition_and_indexes.sql
@@ -98,3 +98,17 @@ JOIN sys.columns col ON ic.object_id = col.object_id and ic.column_id = col.colu
 JOIN sys.tables t ON ind.object_id = t.object_id 
 WHERE t.name IN ('Order', 'Invoice')
 ORDER BY t.name, ind.name, ind.index_id, ic.is_included_column, ic.key_ordinal;
+
+-- Check number of rows in each partition
+SELECT DISTINCT o.name as table_name, rv.value as partition_range, fg.name as file_groupName, p.partition_number, p.rows as number_of_rows
+FROM sys.partitions p
+JOIN sys.indexes i ON p.object_id = i.object_id AND p.index_id = i.index_id
+JOIN sys.objects o ON p.object_id = o.object_id
+JOIN sys.system_internals_allocation_units au ON p.partition_id = au.container_id
+JOIN sys.partition_schemes ps ON ps.data_space_id = i.data_space_id
+JOIN sys.partition_functions f ON f.function_id = ps.function_id
+JOIN sys.destination_data_spaces dds ON dds.partition_scheme_id = ps.data_space_id AND dds.destination_id = p.partition_number
+JOIN sys.filegroups fg ON dds.data_space_id = fg.data_space_id 
+LEFT JOIN sys.partition_range_values rv ON f.function_id = rv.function_id AND p.partition_number = rv.boundary_id
+WHERE o.object_id = OBJECT_ID('Order')
+	OR o.object_id = OBJECT_ID('Invoice');
